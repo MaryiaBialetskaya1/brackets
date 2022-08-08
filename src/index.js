117dab42e05a853546f5557331b1fba98cac62e6
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-08-08 16:59:14 -0400

    feat: add solution to task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = bracketsConfig.map(x => x.join(''));
+  let i = 0;
+  while (i < array.length) {
+    if (str.indexOf(array[i]) != -1) {
+      str = str.replace(array[i], '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+    return (str.length === 0);
 }
